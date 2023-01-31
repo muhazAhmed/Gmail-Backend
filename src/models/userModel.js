c3037c7fcf816c14940b3ed0f5c0c227cedb660d
Muhaz <muhazvla313@gmail.com>
2023-01-31 20:51:22 +0530

    added user model

---
+++ src/models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    phone: { type: Number },
+    recoveryEmail: { type: String },
+    dob: { type: String, required: true },
+    gender: { type: String, enum: ["male", "female", "other"] },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", userSchema);
